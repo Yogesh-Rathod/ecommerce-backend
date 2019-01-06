d4e000e06c48087d807b91cc2664ac3b5246cfab
Yogesh Rathod <yogesh.rathod@loylty.in>
2019-01-06 17:28:26 +0530

    Email unique code changes

--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -1,48 +1,63 @@
 import * as express from 'express';
+
 import Controller from '../interfaces/controller.interface';
 import AddUserDTO from './users.dto';
 import UserInterface from './usersInterface';
-import * as postgres from 'pg';
-import dbConfig from '../config/config';
+import query from '../utils/database';
+import validationMiddleware from '../middlewares/validation.middleware';
+import UserWithThatEmailAlreadyExistsException from '../middlewares/UserWithThatEmailAlreadyExistsException';
 
 class UserController implements Controller {
     public path = '/users';
     public router = express.Router();
-    private client;
 
     constructor() {
-        this.connectToDatabase();
         this.initializeRoutes();
-    }
-
-    private async connectToDatabase() {
-        this.client = new postgres.Pool(dbConfig);
-        await this.client.connect();
     }
 
     private initializeRoutes() {
         this.router.get(`${this.path}/getAllUsers`, this.getAllUsers);
-        this.router.post(`${this.path}/`, this.addUser);
+        this.router.post(
+            `${this.path}/`,
+            validationMiddleware(AddUserDTO),
+            this.addUser
+        );
     }
 
     private getAllUsers = async (
         req: express.Request,
         res: express.Response
     ) => {
-        const { rows } = await this.client.query('SELECT * FROM users');
-        res.send(rows);
+        const { rows } = await query('SELECT * FROM users');
+        return res.send(rows);
     };
 
-    private addUser = async (req: express.Request, res: express.Response) => {
-        // console.log('req ');
-        const userData: AddUserDTO = req.body;
-        // try {
-
-        // } catch (error) {
-
-        // }
-        // const { rows } = await this.client.query('SELECT * FROM users');
-        res.send(userData);
+    private addUser = async (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) => {
+        try {
+            const userData: AddUserDTO = req.body;
+            const queryString = `SELECT * from users WHERE data->>'email'=$1`,
+                values = [req.body.email];
+            const { rows } = await query(queryString, values);
+            if (rows && rows.length) {
+                console.log('await ');
+                next(
+                    new UserWithThatEmailAlreadyExistsException(userData.email)
+                );
+            } else {
+                const queryString =
+                    'INSERT INTO users(data) VALUES($1) RETURNING *';
+                const values = [userData];
+                const { rows } = await query(queryString, values);
+                res.status(200).send(rows);
+            }
+        } catch (error) {
+            console.log('error ', error);
+            res.status(400).send(error);
+        }
     };
 }
 
